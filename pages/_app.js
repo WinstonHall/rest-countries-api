e0c8fcfef19ef5f39f33c82fde83d3d8ab1425e9
Winston <WLHDeveloper@gmail.com>
2022-11-22 19:47:44 -0500

    -Attempting to use Google's fonts, not working or final.

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import '../styles/globals.css'
 import HeadBar from "../components/HeadBar";
+import {Nunito} from '@next/font/google'
+//TODO: See if next/font/google even works
+//      if not then remove it and find a way to import the fonts you need.
+const nunito = Nunito({subset: ['latin']})
 
 function MyApp({Component, pageProps}) {
     return <>
-        <HeadBar/>
-        <Component {...pageProps} />
+        <main className={nunito.className}>
+            <HeadBar/>
+            <Component {...pageProps} /></main>
     </>
 }
 
